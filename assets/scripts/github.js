a1c31e8a2eb8def00b1f32cddbf2e7f1967f87aa
Stuart Saint <stuartsaint242@gmail.com>
2020-09-13 15:25:29 +0000

    Bugfixes and default behaviour

--- assets/scripts/github.js
+++ assets/scripts/github.js
@@ -27,15 +27,17 @@ function repoInformationHTML(repos) {
     });
 
     return `
-    <div class="clearfix repo-list">
-        <p>Repo List</p>
-        <ul>
-            ${listItemsHTML.join("/n")}
-        </ul>
-    </div>`;
+        <div class="clearfix repo-list">
+            <p>
+                <strong>Repo List:</strong>
+            </p>
+            <ul>${listItemsHTML.join("\n")}</ul>
+        </div>`;
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -71,3 +73,5 @@ function fetchGitHubInformation(event) {
         }
     )
 }
+
+$(document).ready(fetchGitHubInformation);
